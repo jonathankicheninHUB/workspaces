2f37e2aac78d120524064563c27d34b909121a4d
jonathan kichenin <jonathan.kichenin@gmail.com>
2025-12-23 18:29:38 +0100

    Update config.js

--- dov-command-v4/src/data/config.js
+++ dov-command-v4/src/data/config.js
@@ -1,1 +1,26 @@
-
+export const CONFIG = {
+    PRIX: {
+        WAX_USD: 0.00751, 
+        DOVX_WAX: 0.00528553,
+        DOVR_WAX: 0.00052051,
+        DOVF_WAX: 0.00052051,
+        DOVH_WAX: 0.00052051,
+        DOVS_WAX: 0.00052051,
+        PRIX_MOYEN_NFT_USD: 5 
+    },
+    API: {
+        NODES: [
+            "https://wax.greymass.com", "https://wax.eosio.online", "https://wax.api.eosnation.io",
+            "https://wax.dapplica.io", "https://wax.eosphere.io", "https://wax.eosusa.io",
+            "https://wax-public1.neftyblocks.com", "https://wax-public2.neftyblocks.com",
+            "https://wax.eosrio.io", "https://api.wax.detroitledger.tech", "https://wax.oiac.io",
+            "https://api.hivebp.io", "https://api2.hivebp.io", "https://api-wax.eosauthority.com",
+            "https://wax.eosdac.io", "https://wax.blacklusion.io", "https://api.waxsweden.org",
+            "https://wax-bp.wizardsguild.one", "https://api.wax.alohaeos.com", "https://wax.eu.eosamsterdam.net"
+        ]
+    },
+    LINKS: [
+        { name: "Official Website", url: "https://dawnofvictory.io/" },
+        { name: "Whitepaper", url: "https://dawn-of-victory.gitbook.io/dov/" }
+    ]
+};
